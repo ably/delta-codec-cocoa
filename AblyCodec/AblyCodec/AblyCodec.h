32ede8ff7df491000457aadcbf8d7e4ac6fad0d3
Ricardo Pereira <m@ricardopereira.eu>
2019-09-05 14:18:49 +0100

    Project init

---
+++ AblyCodec/AblyCodec/AblyCodec.h
@@ -0,0 +1,19 @@
+//
+//  AblyCodec.h
+//  AblyCodec
+//
+//  Created by Ricardo Pereira on 05/09/2019.
+//  Copyright © 2019 Ably. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AblyCodec.
+FOUNDATION_EXPORT double AblyCodecVersionNumber;
+
+//! Project version string for AblyCodec.
+FOUNDATION_EXPORT const unsigned char AblyCodecVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AblyCodec/PublicHeader.h>
+
+
