00f2d53a7a2a2892e386c9ca2d72cf7822c3f13d
Ricardo Pereira <ricardopereira@whitesmith.co>
2019-09-13 10:48:32 +0100

    Add tests

--- AblyCodecTests/AblyCodecTests.m
+++ AblyCodecTests/AblyCodecTests.m
@@ -14,26 +14,77 @@
 
 @end
 
-@implementation AblyCodecTests
+@implementation AblyCodecTests {
+    NSString *base;
+    NSData *baseData;
+    NSString *deltaBase64;
+    NSData *deltaData;
+    NSString *expectedDeltaOutput;
+}
 
 - (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    base = @"fhgj fhgj input0 fhgj fhgj";
+    baseData = [base dataUsingEncoding:NSUTF8StringEncoding];
+
+    deltaBase64 = @"1sPEAAABGgAQHgAFBAIxIDEgTx8CGgUAEA==";
+    deltaData = [[NSData alloc] initWithBase64EncodedString:deltaBase64 options:NSDataBase64DecodingIgnoreUnknownCharacters];
+
+    expectedDeltaOutput = @"fhgj fhgj input1 fhgj fhgj 1 O";
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
 }
 
-- (void)testExample {
-    ARTCodec *codec = [[ARTCodec alloc] init];
-    [codec test];
+- (void)testIsDelta {
+    XCTAssertTrue([ARTDeltaCodec isDelta:deltaData]);
+
+    deltaData = [[NSData alloc] initWithBase64EncodedString:@"foo" options:0];
+    XCTAssertFalse([ARTDeltaCodec isDelta:deltaData]);
+
+    deltaData = nil;
+    XCTAssertFalse([ARTDeltaCodec isDelta:deltaData]);
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testBasicDecode {
+    NSError *error;
+    ARTDeltaCodec *codec = [[ARTDeltaCodec alloc] init];
+    [codec setBase:baseData withId:@"m1"];
+
+    NSData *outputData = [codec applyDelta:deltaData deltaId:@"m2" baseId:@"m1" error:&error];
+    NSString *output = [[NSString alloc] initWithData:outputData encoding:NSUTF8StringEncoding];
+
+    XCTAssertNil(error);
+    XCTAssertTrue([output isEqualToString:expectedDeltaOutput]);
+}
+
+- (void)testDecodeWithNoBase {
+    NSError *error;
+    ARTDeltaCodec *codec = [[ARTDeltaCodec alloc] init];
+    NSData *output = [codec applyDelta:[NSData new] deltaId:@"m2" baseId:@"m1" error:&error];
+    XCTAssertNil(output);
+    XCTAssertNotNil(error);
+    XCTAssertEqual(error.code, ARTDeltaCodecCodeErrorUninitializedDecoder);
+}
+
+- (void)testDecodeWithInvalidDelta {
+    NSError *error;
+    ARTDeltaCodec *codec = [[ARTDeltaCodec alloc] init];
+    [codec setBase:baseData withId:@"m1"];
+    NSData *output = [codec applyDelta:[NSData new] deltaId:@"m2" baseId:@"m1" error:&error];
+    XCTAssertNil(output);
+    XCTAssertNotNil(error);
+    XCTAssertEqual(error.code, ARTDeltaCodecCodeErrorInvalidDeltaData);
+}
+
+- (void)testDecodeWithMismatchBaseId {
+    NSError *error;
+    ARTDeltaCodec *codec = [[ARTDeltaCodec alloc] init];
+    [codec setBase:baseData withId:@"m1"];
+    NSData *output = [codec applyDelta:[NSData new] deltaId:@"m2" baseId:@"foo" error:&error];
+    XCTAssertNil(output);
+    XCTAssertNotNil(error);
+    XCTAssertEqual(error.code, ARTDeltaCodecCodeErrorBaseIdMismatch);
 }
 
 @end
